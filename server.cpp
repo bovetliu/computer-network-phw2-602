76cba684ad1210d935fa796b338ff14c1248b44d
bovetliu <bowei_liu@ymail.com>
2015-10-11 18:59:12 -0500

    only use new_addr_in, client_addr_in, tmp_addr_in

--- server.cpp
+++ server.cpp
@@ -36,27 +36,21 @@ using namespace std;
 int main(int argc, char *argv[]){
     int error;
     struct sigaction sa;
-    struct sockaddr_storage client_addr, tmp_addr;
-    socklen_t addr_len, tmp_len;
+    //struct sockaddr_storage client_addr, tmp_addr;
     char ipstr[INET_ADDRSTRLEN];        //INET6_ADDRSTRLEN for IPv6
     connection_info server_conn_info;
     connection_info *clients_conn_info =(connection_info*)malloc(MAX_CLIENTS*sizeof(connection_info));
 
     initialize_server(argc, argv, server_conn_info);
     int sockfd = server_conn_info.sockfd;
-
-
     // Initializing the required variables
     char buf[1024];
     int num_bytes;
 
-    struct sockaddr new_addr;
-    new_addr = *(server_conn_info.address_info.ai_addr);
-
-    struct sockaddr_in* p_new_addr_in;
-    p_new_addr_in = (struct sockaddr_in*) &new_addr;
-    p_new_addr_in->sin_port = htons(0); // so can have random new available port
-
+    struct sockaddr_in new_addr_in, client_addr_in, tmp_addr_in;
+    socklen_t addr_len, tmp_len;
+    new_addr_in = server_conn_info.address;
+    new_addr_in.sin_port = htons(0); // so can have random new available port
 
     //Bowei, do not understand here
     sa.sa_handler = sigchld_handler; // reap all dead processes
@@ -75,15 +69,15 @@ int main(int argc, char *argv[]){
 
     printf("Server Running ...\n");
     while(1){
-        addr_len = sizeof client_addr;
-        if ((num_bytes = recvfrom(sockfd, buf, 1023 , 0,(struct sockaddr *)&client_addr, &addr_len)) == -1) {
+        addr_len = sizeof client_addr_in;
+        if ((num_bytes = recvfrom(sockfd, buf, 1023 , 0,(struct sockaddr *)&client_addr_in, &addr_len)) == -1) {
             perror("recvfrom");
             exit(1);
         }
-        struct sockaddr_in* new_client_a = (struct sockaddr_in*)&client_addr;
+
         printf("Server: got packet from %s on port %hu\n",
-            inet_ntop(client_addr.ss_family,get_in_addr((struct sockaddr *)&client_addr),ipstr, sizeof ipstr),
-            ntohs(new_client_a->sin_port)
+            inet_ntop(client_addr_in.sin_family,get_in_addr((struct sockaddr *)&client_addr_in),ipstr, sizeof ipstr),
+            ntohs(client_addr_in.sin_port)
         );
         //printf("Server: packet is %d bytes long\n", num_bytes);
 
@@ -104,16 +98,16 @@ int main(int argc, char *argv[]){
                 perror("server: socket");
             }
             // bind this sockfd with port 0, ( )
-            if((error = bind(sockfd, &new_addr, sizeof(new_addr)))== -1){     // bind the server to the IP address and port 0, port 0 is important
+            if((error = bind(sockfd, (struct sockaddr *)&new_addr_in, sizeof(new_addr_in)))== -1){     // bind the server to the IP address and port 0, port 0 is important
                 perror("server: bind");
             }
 
-            socklen_t len = sizeof(new_addr);
+            addr_len = sizeof(new_addr_in);
             //http://stackoverflow.com/questions/1365265/on-localhost-how-to-pick-a-free-port-number
-            if (getsockname(sockfd, &new_addr, &len) == -1) {
+            if (getsockname(sockfd,(struct sockaddr * ) &new_addr_in, &addr_len) == -1) {
                 perror("getsockname");
             }
-            printf("New Client Port %hu\n",ntohs(p_new_addr_in->sin_port));
+            printf("New Client Port %hu\n",ntohs(new_addr_in.sin_port));
 
             /***********************************************************************************/
             // Opening requested file and send ERROR message if file could not be opened
@@ -130,7 +124,7 @@ int main(int argc, char *argv[]){
                 int emsg_len = strlen(emsg);
                 char *epacket = encode(ERROR,1,emsg,emsg_len);
                 free(emsg);
-                if( (num_bytes = sendto(sockfd,epacket,emsg_len+5,0,(struct sockaddr *)&client_addr,addr_len)) == -1 ){
+                if( (num_bytes = sendto(sockfd,epacket,emsg_len+5,0,(struct sockaddr *)&client_addr_in,addr_len)) == -1 ){
                     perror("server: sendto");
                     exit(1);
                 }
@@ -149,7 +143,7 @@ int main(int argc, char *argv[]){
             int num_packets = ((last - first) /512) + 1;           // Calculate the number of packets to be sent
             myfile.seekg(0,ios::beg); // set the position of input sequence back
 
-            cout << "File Size = " << last - first<< ", Packets = " << num_packets << ", New Port = " << ntohs(p_new_addr_in->sin_port) << endl;
+            cout << "File Size = " << last - first<< ", Packets = " << num_packets << ", New Port = " << ntohs(new_addr_in.sin_port) << endl;
 
             char file_buf[513];
             int blocknumber = 0, last_ack = 0, resend = 0, m_len;
@@ -177,7 +171,7 @@ int main(int argc, char *argv[]){
                     cout << "Resending Attempt " << resend << " for Blocknumber " << blocknumber << endl;
                 }
                 char *packet = encode(DATA,blocknumber,file_buf,m_len);                       // Encode the data into the TFTP packet structure
-                if( (num_bytes = sendto(sockfd,packet,m_len+4,0,(struct sockaddr *)&client_addr,addr_len)) == -1 ){ // Send Data Packet
+                if( (num_bytes = sendto(sockfd,packet,m_len+4,0,(struct sockaddr *)&client_addr_in,addr_len)) == -1 ){ // Send Data Packet
                     perror("server: sendto");
                     exit(1);
                 }
@@ -190,13 +184,12 @@ int main(int argc, char *argv[]){
                     exit(4);
                 }
                 if(FD_ISSET(sockfd,&read_fds)){
-                    tmp_len = sizeof tmp_addr;
-                    if ( (num_bytes = recvfrom(sockfd, buf, 1023 , 0,(struct sockaddr *)&tmp_addr, &tmp_len)) == -1 ) {        // receive ACK
+                    tmp_len = sizeof tmp_addr_in;
+                    if ( (num_bytes = recvfrom(sockfd, buf, 1023 , 0,(struct sockaddr *)&tmp_addr_in, &tmp_len)) == -1 ) {        // receive ACK
                         perror("recvfrom");
                         exit(1);
                     }
-                    struct sockaddr_in* tmp_client_a = (struct sockaddr_in*)&tmp_addr;
-                    if(tmp_client_a->sin_addr.s_addr == new_client_a->sin_addr.s_addr){
+                    if(tmp_addr_in.sin_addr.s_addr == client_addr_in.sin_addr.s_addr){
                         p_rec_tftpR = decode(buf);                                      // decode ACK message
                         // need some conditional block to know whether this is an ACK or ERROR
 
